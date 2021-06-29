896d7cbab3bfd5bdb08847ca2a42ad38aaa7c2d3
jestinboby <jestinboby@gmail.com>
2021-06-29 23:27:05 +0530

    json

--- correlation.py
+++ correlation.py
@@ -1,55 +1,46 @@
 import json
-from math import sqrt
+import math
 
-def load_journal(journal):
- f = open(journal)
- data = json.load(f)
- return data
+def load_journal(filename):
+	f=open(filename,)
+	data = json.load(f)
+	f.close()
+	return data
 
-def compute_phi(journal,event):
-  phi = 0
-  data = load_journal(journal)
-  var1,var2,var3,var4,var5,var6,var7,var8 = 0,0,0,0,0,0,0,0
-  for i in data:
-      if event in i['events']:
-          var5 += 1
-          if i['squirrel']==True:
-              var1 += 1
-              var7 += 1
-          else:
-              var3 += 1
-              var8 += 1
-  else:
-          var6 += 1
-          if i['squirrel']==True:
-                var7 += 1
-                var4 += 1
-          else:
-                var8 += 1
-                var2 += 1
-  phi = (var1*var2)-(var3*var4)/sqrt(var5*var6*var7*var8)
-  return phi
+def compute_phi(filename,event):
+	data = load_journal(name)
+	n11=n00=n10=n01=n1p=n0p=np1=np0=0
+	for i in data:
+		if event in i['events']:
+			n1p += 1
+			if i['squirrel'] == True:
+				np1 += 1
+				n11 += 1
+			else:
+				np0 += 1
+				n10 += 1
+		else:
+			n0p += 1
+			if i['squirrel'] == True:
+				np1 += 1
+				n01 += 1
+			else:
+				np0 += 1
+				n00 += 1
 
-def compute_correlations(journal):
-  data = load_journal(journal)
-  corr_dict = {}
-  for i in data:
-      for j in i['events']:
-         corr_val = compute_phi(journal,j)
-         if j not in corr_dict:
-             corr_dict[j]=corr_val
-  return corr_val
+	return (n11*n00 - n10*n01) / math.sqrt(n1p*n0p*np1*np0)
 
-def diagnose(journal):
-    corr_dict=compute_correlations(journal)
-    max = min = 0
-    for key,value in corr_dict.items():
-        if value >= 0:
-            if value > max:
-                max = value
-                max_key = max_key
-        else:
-            if value < min:
-                min = value
-                min_key = max_key
-    return max_key,min_key
+def compute_correlations(filename):
+	data = load_journal(filename)
+	corr_dic = {}
+	for i in data:
+		for event in i['events']:
+			if event not in dic.keys():
+				corr_dic[event] = compute_phi(filename,event)
+	return corr_dic
+
+def diagnose(filename):
+	dic = compute_correlations(filename)
+	max_value=max(dic, key=dic.get)
+	min_value=min(dic, key=dic.get)
+	return max_value,min_value
